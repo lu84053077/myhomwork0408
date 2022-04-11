be2748b0f1e3c3a34f03c143d94b33e335447322
lu84053077 <103295953+lu84053077@users.noreply.github.com>
2022-04-11 09:04:13 +0800

    Add files via upload

---
+++ Operation9/Operation9/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace Operation9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 36);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 36);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Rows";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "直角聖誕樹";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(403, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(261, 335);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 54);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "100的2進位是7";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(27, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 67);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "樂透";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button6.Location = new System.Drawing.Point(228, 104);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(123, 94);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "奇數 有幾個 , 偶數有幾個 ?";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(228, 299);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 93);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "名字有 “C” or “c” 字樣有幾個 ?";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button7.Location = new System.Drawing.Point(217, 219);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(121, 74);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "最長名子";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(217, 414);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(134, 80);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "最高分 ，最低分";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1733, 900);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Operation9/Operation9/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Operation9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { int c = 1;
+            for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    richTextBox1.Text += "*";
+
+
+                }
+                c++;
+                richTextBox1.Text += "\n";
+            }
+            }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = 100;
+            int c = 0;
+            List<int> a = new List<int>();
+            while (i > 1)
+            {
+
+
+                c++;
+                a.Add(i % 2);
+
+                i = i / 2;
+            }
+            a.Add(i);
+           
+
+            for (int j = c; j >=0; j--)
+            {
+                richTextBox1.Text += a[j].ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 6; i++) {
+
+                richTextBox1.Text += rnd.Next(49);
+                richTextBox1.Text += "  ";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            String[] names = { "aaa", "bbb", "ccc", "Mary", "Tom" };
+            string c = "";
+            int a = 0, b = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (i < 4)
+                {
+
+                    c += names[i] + " , ";
+                }
+
+                else
+                {
+                    c += names[i];
+                }
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+
+                if (a < names[i].Length)
+                {
+                    a = names[i].Length;
+                    b = i;
+                }
+            }
+
+
+            MessageBox.Show("neame :(" + c + ")" + "\n最長名子為 : " + names[b]);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int[] names = { 44, 66, 99, 100, 78 };
+            string c = "";
+            int a = 0, b = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (i < 4)
+                {
+
+                    c += names[i] + " , ";
+                }
+
+                else
+                {
+                    c += names[i];
+                }
+
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+
+
+                if (names[i] % 2 == 1)
+                    a++;
+                else
+                    b++;
+
+            }
+            MessageBox.Show("name : (" + c + ")" + "\n 奇數有 : " + a + "\n偶數有 : " + b);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            String[] names = { "aaa", "bbb", "ccc", "Mary", "Tom" };
+            string c = "";
+            int z = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (i < 4)
+                {
+
+                    c += names[i] + " , ";
+                }
+
+                else
+                {
+                    c += names[i];
+                }
+
+            }
+
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                if (names[i].Contains("C") || names[i].Contains("c"))
+                {
+                    z++;
+                }
+
+            }
+
+            MessageBox.Show("neame :(" + c + ")" + "\n名字有 “C” or “c” 字樣有 : " + z + "個");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int[] names = { 44, 66, 99, 100, 78 };
+            string c = "";
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (i < 4)
+                {
+
+                    c += names[i] + " , ";
+                }
+
+                else
+                {
+                    c += names[i];
+                }
+
+            }
+            MessageBox.Show("name : (" + c + ")" + "\n最高分 :" + names.Max() + "\n最低分 : " + names.Min());
+        }
+    }
+}
